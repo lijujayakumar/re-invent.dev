6777501e6b45a7d2200dcd977dc74840b031c78b
Liju Jayakumar <lijayaku@amazon.com>
2023-11-13 19:44:00 -0800

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <header>
+        <h1> re-invent.dev</h1>
       </header>
+      <div className="App">
+        <h2>Welcome to re-invent.dev!</h2>
+        <p>
+          Together, let's re-invent the future of development.
+        </p>
+      </div>
     </div>
   );
 }
 
-export default App;
+export default App; 
